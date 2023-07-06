0048954b2de152853340e9acd350c5136d152abd
codestove <shine@codestove.io>
2023-07-06 23:46:48 +0900

    head, footer html 모듈로 나누기.

--- my_first_django/settings.py
+++ my_first_django/settings.py
@@ -121,8 +121,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp/templates/static'),
+]
 
-STATIC_URL = "static/"
+STATICFILES = os.path.join(BASE_DIR, 'static')
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

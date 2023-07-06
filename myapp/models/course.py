5cd016654e56a75ac990daa638a7e17e4ca173c1
codestove <shine@codestove.io>
2023-07-06 21:30:12 +0900

    model change

--- myapp/models/course.py
+++ myapp/models/course.py
@@ -12,7 +12,7 @@ class Course(models.Model):
     professor = models.CharField(max_length=100)
     limit = models.IntegerField(default=0)
     description = models.CharField(max_length=300)
-    permit_subject = models.JSONField(default=dict)
+    permit_subject = models.JSONField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
